fbbdea58503bea12282eda7454838b6d24a2c8bf
StAlexN <StAlexN@DESKTOP-1QU0RK4>
2023-01-25 13:01:42 +0500

    Добавьте файлы проекта.

---
+++ teacher/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teacher
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            for (int i = 0; i < a.Split(' ').Length; i++) Console.WriteLine(a.Split(' ')[i]);
+
+        }
+    }
+}
